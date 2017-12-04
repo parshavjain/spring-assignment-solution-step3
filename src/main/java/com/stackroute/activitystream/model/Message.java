a05b45148ea6c13f51452ba7e75fe8de8847af0d
Hobbes <hobbes@stackroute.in>
2017-12-04 11:43:42 +0530

    Update Message.java

--- src/main/java/com/stackroute/activitystream/model/Message.java
+++ src/main/java/com/stackroute/activitystream/model/Message.java
@@ -14,21 +14,24 @@ import javax.persistence.Table;
  * note that this class is annotated with @Entity annotation. Hibernate will scan all package for 
  * any Java objects annotated with the @Entity annotation. If it finds any, then it will begin the 
  * process of looking through that particular Java object to recreate it as a table in your database.
+ * 
+ * Please note that you will have to use @Component annotation on this class if wish
+ * to autowire the class from any other components of the application
  */
 
 public class Message {
 
 	/*
 	 * This class should have eight fields
-	 * (messageId,senderName,receiverId,circleName,postedDate,streamType,message,tag). Out of these four fields, the
+	 * (messageId,senderName,receiverId,circleName,postedDate,streamType,message,tag). Out of these eight fields, the
 	 * field messageId should be auto-generated. This class should also contain
 	 * the getters and setters for the fields. The value of postedDate should
 	 * not be accepted from the user but should be always initialized with the
 	 * system date
 	 */
 	
-	public Message(String string, Object object, Object object2, Object object3, String string2, String string3,
-			Object object4) {
+	public Message(String senderName, String receiverId, String circleName, Timestamp postedDate, String streamType,
+			String message, String tag) {
 		// TODO Auto-generated constructor stub
 	}
 
