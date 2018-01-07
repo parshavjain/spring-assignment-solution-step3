28d94ab81e5ab4fafe2b9484139284928d7553a7
parsav.jain <parsav.jain@wipro.com>
2018-01-07 18:38:54 +0530

    AOP

--- src/main/java/com/stackroute/activitystream/config/ApplicationContextConfig.java
+++ src/main/java/com/stackroute/activitystream/config/ApplicationContextConfig.java
@@ -62,22 +62,22 @@ public class ApplicationContextConfig {
 	 * 4. Password
 	 */
 	@Bean(name = "dataSource")
-	public DataSource getDataSource() {
+	/*public DataSource getDataSource() {
 		BasicDataSource dataSource = new BasicDataSource();		
 		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
 	    dataSource.setUrl("jdbc:mysql://localhost:3306/activitystream_step3");
 	    dataSource.setUsername("root");
 	    dataSource.setPassword("P@ssw0rd");
 	    return dataSource;
-	}
-	/*public DataSource getDataSource() {
+	}*/
+	public DataSource getDataSource() {
 		BasicDataSource dataSource = new BasicDataSource();		
 		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
 	    dataSource.setUrl("jdbc:mysql://localhost:3306/" + System.getenv("MYSQL_DATABASE"));
 	    dataSource.setUsername(System.getenv("MYSQL_USER"));
 	    dataSource.setPassword(System.getenv("MYSQL_PASSWORD"));
 	    return dataSource;
-	}*/
+	}
 	
 	
 	/**
